66174ed2e52aacf7160bcec9b736ff034286a65c
crownwangguan <crown.wg@outlook.com>
2018-10-25 21:43:02 -0400

    pymongo, use header list, zip data to create dictionary datastructure, then collection insert data

---
+++ mongo_test.py
@@ -0,0 +1,33 @@
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+# db = client['test-database']
+
+# collection = db['test-collection']
+
+# test_data = {'age': 25, 'gender': 'M', 'occupation': 'Software Engineer'}
+# insert_result = collection.insert_one(test_data)
+
+# print(insert_result)
+# print(collection.count())
+
+# delete_result = collection.delete_one({'age': 25})
+# print(collection.count())
+
+db = client['chapter3']
+collection = db['income']
+
+with open('income_header.txt') as f_in:
+	columns_headings = f_in.readline()
+
+columns_headings_list = columns_headings.split(',')
+
+row_dict_list = list()
+with open('income.txt') as f_in:
+	for line in f_in:
+		row_list = line.rstrip('\n').split(',')
+		row_dict = dict(zip(columns_headings_list, row_list))
+		row_dict_list.append(row_dict)
+		collection.insert_one(row_dict)
+
+print(collection.count())
